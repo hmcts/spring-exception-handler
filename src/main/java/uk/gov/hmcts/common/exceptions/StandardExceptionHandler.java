d8b88a1d969c22b27f8f822a255f4891d6913a29
benedwards <ben.edwards@cgi.com>
2025-08-01 14:38:47 +0100

    Main implementation

--- src/main/java/uk/gov/hmcts/common/exceptions/StandardExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/common/exceptions/StandardExceptionHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.common.exceptions;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 @RequiredArgsConstructor
+@ConditionalOnProperty(
+    prefix = "hmcts.common.exceptions",
+    name = "enable-exception-handler",
+    havingValue = "true",
+    matchIfMissing = true
+)
 public class StandardExceptionHandler {
 
 
